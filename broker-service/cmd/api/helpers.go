ad5ccb5b07a6ab0a7cc16aa86574d45f9ac89ce4
ilya <misyurailya97@gmail.com>
2024-12-14 20:19:30 +0200

    add services

--- broker-service/cmd/api/helpers.go
+++ broker-service/cmd/api/helpers.go
@@ -8,17 +8,19 @@ import (
 )
 
 type jsonResponse struct {
-	Error   bool   `json:"error`
+	Error   bool   `json:"error"`
 	Message string `json:"message"`
 	Data    any    `json:"data,omitempty"`
 }
 
+// readJSON tries to read the body of a request and converts it into JSON
 func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) error {
 	maxBytes := 1048576 // one megabyte
+
 	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
 	dec := json.NewDecoder(r.Body)
 	err := dec.Decode(data)
-
 	if err != nil {
 		return err
 	}
@@ -29,9 +31,9 @@ func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) er
 	}
 
 	return nil
-
 }
 
+// writeJSON takes a response status code and arbitrary data and writes a json response to the client
 func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
 	out, err := json.Marshal(data)
 	if err != nil {
@@ -46,7 +48,6 @@ func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, header
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
-
 	_, err = w.Write(out)
 	if err != nil {
 		return err
@@ -55,8 +56,11 @@ func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, header
 	return nil
 }
 
+// errorJSON takes an error, and optionally a response status code, and generates and sends
+// a json error response
 func (app *Config) errorJSON(w http.ResponseWriter, err error, status ...int) error {
 	statusCode := http.StatusBadRequest
+
 	if len(status) > 0 {
 		statusCode = status[0]
 	}
